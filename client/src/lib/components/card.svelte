ce6da36939d8dafc715ac78dbbe932188fbcb3bd
solypsismo <nomec505@gmail.com>
2024-08-13 18:03:25 +0200

    bozza pagina iniziale

---
+++ client/src/lib/components/card.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let text;
+</script>
+
+<div class="w-full p-4 font-medium bg-secondary text-primary">
+    {text}
+</div>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-screen h-screen bg-slate-300 flex justify-center items-center gap-10">
+
+    <div class="w-1/3 h-2/3 bg-red-500 p-5">
+        <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+        <div class="flex justify-center items-center p-3">
+            <button class="btn btn-outline">Aggiungi</button>
+        </div>
+    </div>
+
+    <div class="w-1/3 h-2/3 bg-red-500 p-5">
+
+    </div>
+
+
+</div>
