97e714248615744ba3df543c236032c7ec492b19
solypsismo <nomec505@gmail.com>
2024-08-13 19:27:31 +0200

    aggiunta dockerfile per frontend

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,13 +1,36 @@
+<script>
+    let tasks = [];
+
+    const aggiungi = async (event) => {
+        event.preventDefault();
+        const form = event.target;
+
+        const obj = Object.fromEntries(new FormData(form));
+
+        const resJSON = await fetch("http://localhost:8080/api/task", {
+            method: "POST"
+        });
+
+        const res = await resJSON.json();
+
+        if(res.success) {
+            tasks = res.tasks;
+        }
+    }
+</script>
+
 <div class="w-screen h-screen bg-slate-300 flex justify-center items-center gap-10">
 
-    <div class="w-1/3 h-2/3 bg-red-500 p-5">
-        <input type="text" placeholder="Type here" class="input input-bordered w-full" />
-        <div class="flex justify-center items-center p-3">
-            <button class="btn btn-outline">Aggiungi</button>
-        </div>
+    <div class="w-1/3 h-2/3 bg-accent p-5">
+        <form method="POST" on:submit={aggiungi}>
+            <input type="text" placeholder="Type here" name="task" class="input input-bordered w-full" />
+            <div class="flex justify-center items-center p-3">
+                <button class="btn btn-outline">Aggiungi</button>
+            </div>
+        </form>
     </div>
 
-    <div class="w-1/3 h-2/3 bg-red-500 p-5">
+    <div class="w-1/3 h-2/3 bg-accent p-5">
 
     </div>
 
